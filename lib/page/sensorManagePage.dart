cf543456583257c7677953317757dda22fe4de70
houxibiao <15120039879@163.com>
2019-04-18 13:29:28 +0800

    use stack and positioned in the sensorManagePage

--- lib/page/sensorManagePage.dart
+++ lib/page/sensorManagePage.dart
@@ -9,7 +9,6 @@ class SensorManagePage extends StatefulWidget {
 }
 
 class _SensorManagePageState extends State<SensorManagePage> {
-  //List<SensorEntity> sensorlist = new List<SensorEntity>();
   List<SensorInfoBean> sensorInfoList = new List<SensorInfoBean>();
   int currentIndex = 0;
 
@@ -22,75 +21,96 @@ class _SensorManagePageState extends State<SensorManagePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("传感器列表"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-            child: Center(
-              child: FlatButton(
-                color: Colors.lightBlue,
-                textColor: Colors.black45,
-                child: Text("添加传感器"),
-                onPressed: () => Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (BuildContext context) =>
-                            new AddSensorPage())),
-              ),
-            ),
-          ),
-          sensorInfoList.isEmpty
-              ? Container(
-                  child: Center(
-                    child: Text("空"),
-                  ),
-                )
-              : ExpansionPanelList(
-                  expansionCallback: (index, bol) {
-                    _setCurrentIndex(index, bol);
-                  },
-                  children: sensorInfoList.map((item) {
-                    return ExpansionPanel(
-                      isExpanded: item.expandedState,
-                      headerBuilder: (context, isExpanded) {
-                        return new ListTile(
-                          title: Text(item.sensor.sensorName),
-                        );
-                      },
-                      body: new Padding(
-                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          textDirection: TextDirection.ltr,
-                          children: <Widget>[
-                            Text("sensorId:${item.sensor.sensorId}"),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text("RoomId:${item.sensor.parentRoom}"),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text("sensorType:${item.sensor.sensorType}"),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Text(item.sensor.description),
-                            SizedBox(
-                              height: 10,
-                            ),
-                          ],
+        appBar: AppBar(
+          title: Text("传感器列表"),
+        ),
+        body: ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: Stack(
+            fit: StackFit.expand,
+            overflow: Overflow.visible,
+            children: <Widget>[
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 80,
+                top: 0,
+                child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  sensorInfoList.isEmpty
+                      ? Container(
+                          child: Center(
+                            child: Text("空"),
+                          ),
+                        )
+                      : ExpansionPanelList(
+                          expansionCallback: (index, bol) {
+                            _setCurrentIndex(index, bol);
+                          },
+                          children: sensorInfoList.map((item) {
+                            return ExpansionPanel(
+                              isExpanded: item.expandedState,
+                              headerBuilder: (context, isExpanded) {
+                                return new ListTile(
+                                  title: Text(item.sensor.sensorName),
+                                );
+                              },
+                              body: new Padding(
+                                padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  textDirection: TextDirection.ltr,
+                                  children: <Widget>[
+                                    Text("sensorId:${item.sensor.sensorId}"),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text("RoomId:${item.sensor.parentRoom}"),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                        "sensorType:${item.sensor.sensorType}"),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(item.sensor.description),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }).toList(),
                         ),
-                      ),
-                    );
-                  }).toList(),
+                ],
+              ),
+              ),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(10, 20, 10, 15),
+                  child: Center(
+                    child: FlatButton(
+                      color: Colors.lightBlue,
+                      textColor: Colors.black45,
+                      child: Text("添加传感器"),
+                      onPressed: () => Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  new AddSensorPage())),
+                    ),
+                  ),
                 ),
-        ],
-      ),
-    );
+              ),
+            ],
+          ),
+        ));
   }
 
   loadSensorList() async {
